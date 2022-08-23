79fa3e1cb7252443576409da00bb844232fcf191
Graham <grahamisaac8@gmail.com>
2022-08-22 19:49:53 -0500

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -6,13 +6,15 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-  let token	
+  let token, accounts, deployer, receiver
 
 	beforeEach(async () => {
 
 	  const Token = await ethers.getContractFactory('Token')
 	  token = await Token.deploy('Graham Coin', 'GC', '1000000')
-
+	  accounts = await ethers.getSigners()
+	  deployer = accounts[0]
+	  receiver = accounts[1]
 	})	
 
 	describe('Deployment', () => {
@@ -40,6 +42,60 @@ describe('Token', () => {
 		expect(await token.totalSupply()).to.equal(totalSupply)
 
 		})
-	})
 	
+		it('Assigns total supply to deployer', async () => {
+		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+
+		})
+	
+  })
+
+	describe('Sending Tokens', async () => {
+		let amount
+		let transaction
+		let result
+
+		describe('Success', () => {
+
+			beforeEach(async () => {
+      amount = tokens('100')
+      transaction = await token.connect(deployer).transfer(receiver.address, amount)
+			result = await transaction.wait()			
+			})
+
+			it('Transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens('999900'))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+
+		  })
+
+      it('Emits a transfer event', async () => {
+        const eventLog = result.events[0]
+
+        expect(eventLog.event).to.equal('Transfer')
+      
+        const args = eventLog.args
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+
+		})
+
+		describe('Failure', () => {
+
+			it('Rejects insufficient balances', async () => {
+				const invalidAmount = tokens('10000000')
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+ 
+      it('Rejects invalid recipient', async () => {
+      	const amount = tokens('100')
+      	await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
+
+		})
+
+	})
+
 })
